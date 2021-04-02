feedd89b6d42c187d876abbb8682e997d3204a20
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2021-04-02 22:34:34 +0900

    FIX: change only zsh on macOS support,any refactoring

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # dotfiles
+This is git-initset-script ver: 1.0
+
+- Attention notis
+If you alredy setting .zshrc , this script remove your setting .
+
+## Dependency
+macOS Big Sur 11.2.3
+You can use only zsh.
+
+This scripts target shell is only zsh , so if you want to use script for bash , please regulation yourself .
+for example .zshrc → .bashrc
+
+## Usage
++ Open mac laptop or computer.
++ Open terminal.app
++ Clone this Repository.
++ Chenge directory on this Repository.
++ Input this command.
+```
+$ sh git-initset-script.sh
+```
+setting.
+Done.
+
+## License
+Copyright (c) 2021 Kazuya yuda.
+This software is released under the MIT License, see LICENSE.
+https://opensource.org/licenses/mit-license.php
+
+## Authors
+kazuya yuda.
+
+## References
+git-prompt.sh  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
---
+++ git_initset_script.sh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+/bin/echo "************************************************"
+/bin/echo "Welcome to git-initset-script ver: 1.0"
+/bin/echo "Caution! , this script support only macOS's zsh ."
+/bin/echo "************************************************"
+
+<<COMMENT_OUT
+This scripts target shell is only zsh , so if you want to use script for bash , please regulation yourself .
+for example .zshrc → .bashrc
+
+- Attention notis
+If you alredy setting .zshrc , this script remove your setting .
+COMMENT_OUT
+
+/bin/echo "+ Make .zshrc on home"
+touch ~/.zshrc
+cat ./settingfile > ~/.zshrc
+    
+/bin/echo "+ Make dir ~/.zsh/completion (add p option)"
+mkdir -p ~/.zsh/completion/
+
+/bin/echo "+ Dowmload git-prompt.sh"
+curl -LO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+mv ./git-prompt.sh ~/.zsh/completion/
+
+/bin/echo "+ Setting global user.name , user.email" 
+/bin/echo "+ Add whoami username to user.name"
+/bin/echo "*********"
+whoami
+/bin/echo "*********"
+
+# set whoami.
+git config --global user.name `whoami`
+
+/bin/echo "+ Please input setting email ."
+/bin/echo "※You can use private email on github and any git cloud service's private email ."
+/bin/echo -n "» "
+
+read email
+
+# set input email.
+git config --global user.email $email
+
+echo "Done , Please check any setting files yourself ."
+echo "Start your happy Git life . 🍀 "
+
