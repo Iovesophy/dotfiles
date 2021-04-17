dc0e0c57221364fa8550a24a11aa85b92d028b6f
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2021-04-17 12:30:56 +0900

    FIX: append patch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,18 @@
 module.exports = function (grunt) {
-    var pkg = grunt.file.readJSON('package.json');
-    grunt.initConfig({
-      zshlint: {
-        options: { force: false },
-        files: ['/root/.zshrc','/root/.zsh/completion/git-prompt.sh']
-      }
-    });
-    grunt.loadNpmTasks('grunt-zshlint');
-    grunt.registerTask('default', ['zshlint']);
+  grunt.initConfig({
+    zshlint: {
+      options: {
+        force: false
+      },
+      src: ['{{filename}}', '/root/.zshrc', '/root/.zsh/completion/git-prompt.sh']
+    }
+  });
+  grunt.loadNpmTasks('grunt-zshlint');
+  grunt.registerTask('default', ['zshlint']);
+  grunt.registerTask('file', 'filename', function (fileName) {
+    var config = grunt.config();
+    config.zshlint.src[0] = config.zshlint.src[0].replace(/{{filename}}/g, fileName);
+    grunt.initConfig(config);
+    grunt.task.run('zshlint');
+  });
 };
--- test.sh
+++ test.sh
@@ -1,61 +1,65 @@
 #!/bin/bash
 # not support function on sh , so set bash.
-set -eux
+set -eu
 
 printf "************************************************\n"
 printf "Welcome to dotfile-test-script ver: 1.0\n"
 printf "************************************************\n"
 
-function Check()
+
+function StatusCheck()
 {
-    printf "${1}\n"
-    if [ ${1} = 0 ]; then
-        :
-    else
-        printf '\033[31m%s\033[m\n' "Error: code ${1}"
-        exit 0
-    fi
+  printf "${1}\n"
+  if [ ${1} = 0 ]; then
+      :
+  else
+      exit 1
+  fi
 }
 
-cd /root/dotfiles
-./start_setting.sh
-Check $? 
+function FileExec()
+{
+  exectimes=${1}
+  for i in `seq $exectimes`; do
+    ./start_setting.sh
+  done
+}
 
-zshrc_o=`md5sum /root/dotfiles/zshrc | awk '{print $1}'`
-zshrc_b=`md5sum /root/.zshrc | awk '{print $1}'`
-vimrc_o=`md5sum /root/dotfiles/zshrc | awk '{print $1}'`
-vimrc_b=`md5sum /root/.zshrc | awk '{print $1}'`
-if [ $zshrc_o = $zshrc_b -a $vimrc_o = $vimrc_b ]; then
-    :
-else
-    exit 1
-fi
+function FileCheck()
+{
+  zshrc_o=`md5sum zshrc | awk '{print $1}'`
+  zshrc_b=`md5sum ~/.zshrc | awk '{print $1}'`
+  vimrc_o=`md5sum vimrc | awk '{print $1}'`
+  vimrc_b=`md5sum ~/.vimrc | awk '{print $1}'`
 
-vint /root/.vimrc
-Check $? 
+  if [ $zshrc_o = $zshrc_b -a $vimrc_o = $vimrc_b ]; then
+      :
+  else
+      exit 1
+  fi
+}
 
-grunt zshlint
-Check $? 
+FileExec 1
+StatusCheck $?
 
-# 2 times program exec test.
-./start_setting.sh
-./start_setting.sh
-Check $? 
+FileExec 1
+FileCheck
+StatusCheck $?
 
-zshrc_o=`md5sum /root/dotfiles/zshrc | awk '{print $1}'`
-zshrc_b=`md5sum /root/.zshrc | awk '{print $1}'`
-vimrc_o=`md5sum /root/dotfiles/zshrc | awk '{print $1}'`
-vimrc_b=`md5sum /root/.zshrc | awk '{print $1}'`
-if [ $zshrc_o = $zshrc_b -a $vimrc_o = $vimrc_b ]; then
-    :
-else
-    exit 1
-fi
-
-vint /root/.vimrc
-Check $? 
+vint vimrc
+StatusCheck $?
 
 grunt zshlint
-Check $? 
+StatusCheck $?
+
+FileExec 2
+FileCheck
+StatusCheck $?
+
+vint vimrc
+StatusCheck $?
+
+grunt zshlint
+StatusCheck $?
 
 printf '\033[32m%s\033[m\n' 'Done. Test All Clear.'
