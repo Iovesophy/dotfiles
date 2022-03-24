52d7ec2e5453429f75e8c671a237ee21de7d11bd
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-03-24 12:31:09 +0900

    FIX: start_setting: change position of email check

--- start_setting.sh
+++ start_setting.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -eux
 
-cp gitconfig_addon ~/.gitconfig
-git config --global user.name "$(whoami)"
 if [ -z "${1:-""}" ]; then
   exit 1
-else
-  git config --global user.email "$1"
 fi
+
+cp gitconfig_addon ~/.gitconfig
+git config --global user.name "$(whoami)"
+git config --global user.email "$1"
 
 ln -fs "$(pwd)"/zshrc ~/.zshrc
 ln -fs "$(pwd)"/vimrc ~/.vimrc
@@ -14,3 +14,4 @@ ln -fs "$(pwd)"/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload
 cd ~/.vim/autoload
 curl -O https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
