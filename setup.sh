c5c1d8c210a39e8441e3675bb04854d6a49882fc
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-03-25 17:40:11 +0900

    Refactor- gather install phase of vim

--- setup.sh
+++ setup.sh
@@ -11,7 +11,3 @@ git config --global user.email "$1"
 ln -fs "$(pwd)"/zshrc ~/.zshrc
 ln -fs "$(pwd)"/vimrc ~/.vimrc
 
-mkdir -p ~/.vim/autoload
-cd ~/.vim/autoload
-curl -O https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
